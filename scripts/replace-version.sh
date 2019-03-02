956b885963f5812c4e1dfc11591566e1dd641d07
kevin <kjwilde@hotmail.com>
2019-03-02 17:13:55 -0600

    Preparing for local development and deploymnet.

--- scripts/replace-version.sh
+++ scripts/replace-version.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-version=$1
-file=$2
+file=$1
+app=$2
+version=$3
 
 offset=2
 
 pwd
 ls -al
 
-line=$(grep -n "btcd-kube" ${file} | head -n 1 | cut -d: -f1)
+line=$(grep -n ${app} ${file} | head -n 1 | cut -d: -f1)
 
 line=$((${line} + ${offset}))
 
